3d68525d2dd58a000b5a5358ec31c95418dc61d3
khominiatych <hominyatichalbina@gmail.com>
2023-12-07 17:14:35 +0200

    наслідування атрибутів

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 #Наслідування класів
-class Parent:
-    pass
+class Grandparent:
+    height = 170
+    eyes = "green"
+    age = 70
+class Parent(Grandparent):
+    age = 40
 class Child(Parent):
-    pass
+    age = 15
+    height = 165
+    def __init__(self):
+        print(self.height)
+        print(self.eyes)
+        print(self.age)
+
+max = Child()
 #приклад
 class Human:
     height = 170
@@ -14,3 +25,4 @@ class Worker(Human):
 nick = Student()
 kate = Worker()
 print(nick.height, kate.height)
+
