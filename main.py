bd97221a2949842f128347836f3ab6ddb3f2e27e
khominiatych <hominyatichalbina@gmail.com>
2023-12-07 17:04:06 +0200

    батькі і діти

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+#Наслідування класів
 class Parent:
     pass
 class Child(Parent):
     pass
+#приклад
+class Human:
+    height = 170
+class Student(Human):
+    pass
+class Worker(Human):
+    pass
+
+nick = Student()
+kate = Worker()
+print(nick.height, kate.height)
